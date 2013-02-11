0ce9fa2ef51755f6b0f2f17cb46fa22a43e97d74
Zach Wily <zach@zwily.com>
2013-02-11 14:44:18 -0700

    add instructure apt repo

--- cookbooks/base/recipes/default.rb
+++ cookbooks/base/recipes/default.rb
@@ -1,1 +1,15 @@
 package "vim"
+
+execute "apt-get update" do
+  command "apt-get update"
+  action :none
+end
+
+cookbook_file "/etc/apt/sources.list.d/instructure.list" do
+  notifies :run, "execute[apt-get update]", :delayed
+end
+
+cookbook_file "/etc/apt/trusted.gpg.d/apt.insops.net.gpg" do
+  notifies :run, "execute[apt-get update]", :delayed
+end
+
